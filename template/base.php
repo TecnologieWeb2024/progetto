0c8fe97b5593849bd3ac628950c5db39699716d5
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-24 19:03:43 +0100

    Improve navbar, center elements

--- template/base.php
+++ template/base.php
@@ -11,31 +11,34 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark justify-content-center sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">COFFEEBO</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+        <div class="container-fluid d-flex">
+            <div class="d-flex flex-grow-1">
+                <a class="navbar-brand" href="#">COFFEEBO</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+            <div class="collapse flex-grow-1 navbar-collapse justify-content-center collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Prodotti</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Contatti</a></li>
                 </ul>
+            </div>
+            <div class="collapse flex-grow-1 navbar-collapse justify-content-end collapsibleNavbar">
                 <?php if (isUserLoggedIn()) : ?>
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav">
                         <!-- TODO: Usare icone per Carrello e Logout -->
                         <li class="nav-item"><a class="nav-link" href="#">Carrello</a></li>
                         <li class="nav-item"><a class="nav-link logout-link" href="#">Logout</a></li>
                     </ul>
                 <?php else: ?>
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav">
                         <!-- TODO: Usare icone per Login -->
                         <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
                     </ul>
                 <?php endif; ?>
-                
             </div>
         </div>
     </nav>
