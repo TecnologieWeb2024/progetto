9de9b64d15634d17f2a7040064dffcc40f69f2cd
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-25 17:52:04 +0100

    Improve routing and add safety check on requested page

--- index.php
+++ index.php
@@ -3,18 +3,24 @@ require_once("bootstrap.php");
 
 $templateParams["titolo"] = "CoffeeShop - Home";
 
-$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+// Whitelist di pagine per controlli di sicurezza
+$allowedPages = ['products', 'contacts', 'home'];
+$page = isset($_GET['page']) && in_array($_GET['page'], $allowedPages) ? $_GET['page'] : 'home';
 
 switch ($page) {
     case 'products':
         $templateParams["main-content"] = "template/products.php";
+        $templateParams["titolo"] = "CoffeeShop - Prodotti";
         break;
     case 'contacts':
         $templateParams["main-content"] = "template/contacts.php";
+        $templateParams["titolo"] = "CoffeeShop - Contatti";
+
         break;
     case 'home':
     default:
         $templateParams["main-content"] = "template/landing-page.php";
+        $templateParams["titolo"] = "CoffeeShop - Home";
         break;
 }
 require("template/base.php");
