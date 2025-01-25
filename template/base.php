65e2d6120c4fbb82d6fe01401dd30644c43d57b3
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-25 02:43:46 +0100

    remove useless closing tags

--- template/base.php
+++ template/base.php
@@ -2,8 +2,8 @@
 <html lang="it">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $templateParams["titolo"] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
         <div class="container-fluid d-flex">
             <div class="d-flex flex-grow-1">
                 <a class="navbar-brand" href="#">COFFEEBO</a>
