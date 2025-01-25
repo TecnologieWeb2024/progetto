f3b99d33e4f602cd215501ddc290a5bf04b200c1
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-25 17:40:54 +0100

    Use template/products.php inside landing page

--- template/landing-page.php
+++ template/landing-page.php
@@ -5,95 +5,70 @@
             <div class="container d-flex justify-content-center">
                 <div class="row m-2 bg-white border border-3 p-4 rounded">
                     <h4>Ordini recenti</h2>
-                    <?php
-                    // TODO: Ovviamente, i dati devono essere recuperati dal database. Recupera gli ultimi 4 ordini
-                    $orders = [
-                        ["products" => [
-                            ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
-                        ], "total" => "3.00", "date" => "2022-01-01"],
-                        ["products" => [
-                            ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"]
-                        ], "total" => "3.00", "date" => "2022-01-02"],
-                        [
-                            "products" => [
+                        <?php
+                        // TODO: Ovviamente, i dati devono essere recuperati dal database. Recupera gli ultimi 4 ordini
+                        $orders = [
+                            ["products" => [
                                 ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
-                                ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"],
+                                ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
+                                ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
                                 ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
                                 ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
-                            ],
-                            "total" => "4.00",
-                            "date" => "2022-01-03"
-                        ]
-                    ];
-                    ?>
-                    <?php foreach ($orders as $order): ?>
-                        <div class="col-md-4 p-2">
-                            <div class="card mb-4 h-100">
-                                <div class="card-header border border-0">
-                                    <!-- NOTE: Il link deve puntare a order.php?id=... -->
-                                    <a href="#">
-                                        <h6 class="card-title text-center">Ordinato il: <?php echo $order['date']; ?></h5>
-                                    </a>
-                                </div>
-                                <div class="card-body border border-0">
-                                    <div class="row">
-                                        <?php for ($i = 0; $i < 4; $i++): ?>
-                                            <div class="col-6 mb-2">
-                                                <?php if (isset($order['products'][$i])): ?>
-                                                    <?php if ($i == 3 && count($order['products']) > 3): ?>
-                                                        <a href="#" class="d-block h-100">
-                                                            <div class="card h-100 border border-0">
-                                                                <div class="card-body text-center d-flex align-items-center justify-content-center">
-                                                                    Altro...
+                            ], "total" => "3.00", "date" => "2022-01-01"],
+                            ["products" => [
+                                ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
+                                ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"]
+                            ], "total" => "3.00", "date" => "2022-01-02"],
+                            [
+                                "products" => [
+                                    ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
+                                    ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"],
+                                    ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
+                                    ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
+                                ],
+                                "total" => "4.00",
+                                "date" => "2022-01-03"
+                            ]
+                        ];
+                        ?>
+                        <?php foreach ($orders as $order): ?>
+                            <div class="col-md-4 p-2">
+                                <div class="card mb-4 h-100">
+                                    <div class="card-header border border-0">
+                                        <!-- NOTE: Il link deve puntare a order.php?id=... -->
+                                        <a href="#">
+                                            <h6 class="card-title text-center">Ordinato il: <?php echo $order['date']; ?></h5>
+                                        </a>
+                                    </div>
+                                    <div class="card-body border border-0">
+                                        <div class="row">
+                                            <?php for ($i = 0; $i < 4; $i++): ?>
+                                                <div class="col-6 mb-2">
+                                                    <?php if (isset($order['products'][$i])): ?>
+                                                        <?php if ($i == 3 && count($order['products']) > 3): ?>
+                                                            <a href="#" class="d-block h-100">
+                                                                <div class="card h-100 border border-0">
+                                                                    <div class="card-body text-center d-flex align-items-center justify-content-center">
+                                                                        Altro...
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                        </a>
-                                                    <?php else: ?>
-                                                        <img src="<?php echo $order['products'][$i]['image']; ?>" class="img-fluid" alt="<?php echo $order['products'][$i]['name']; ?>">
+                                                            </a>
+                                                        <?php else: ?>
+                                                            <img src="<?php echo $order['products'][$i]['image']; ?>" class="img-fluid" alt="<?php echo $order['products'][$i]['name']; ?>">
+                                                        <?php endif; ?>
                                                     <?php endif; ?>
-                                                <?php endif; ?>
-                                            </div>
-                                        <?php endfor; ?>
+                                                </div>
+                                            <?php endfor; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
                 </div>
             </div>
         </section>
     <?php else: ?>
         <h1>Welcome to CoffeeBo</h1>
     <?php endif; ?>
-    <section>
-        <div class="row">
-            <?php
-            // TODO: Ovviamente, i dati devono essere recuperati dal database
-            $products = [
-                ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
-                ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
-                ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"],
-                ["name" => "Product 4", "price" => "4.00", "image" => "./assets/img/image1.jpg"],
-                ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
-            ];
-
-            foreach ($products as $product): ?>
-                <div class="col-sm-6 col-md-4 col-lg-3">
-                    <div class="card mb-4">
-                        <img src="<?php echo $product['image'] ?>" class="card-img-top" alt="<?php echo $product['name'] ?>">
-                        <div class="card-body border border-0">
-                            <h5 class="card-title"><?php echo $product['name'] ?></h5>
-                            <p class="card-text"><?php echo $product['price'] ?>€</p>
-                            <a href="#" class="btn float-end">Add to Cart</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </section>
+    <?php require("template/products.php"); ?>
 </div>
