01295bf04cb5e0816a2f8f29baafc1073de7a393
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-01-26 22:47:00 +0100

    popolo ruoli e utenti nel db

--- db/dump.sql
+++ db/dump.sql
@@ -114,3 +114,15 @@ CREATE TABLE
         UNIQUE (user_id, product_id) -- Ensures a user cannot have duplicate entries for the same product
     ) ENGINE = InnoDB;
     
+
+-- -----------------------------------------
+-- -- riempio il db
+
+-- inserisco i ruoli
+INSERT INTO `caffebodb`.`roles` (`role_id`, `first_name`) VALUES ('1', 'seller');
+INSERT INTO `caffebodb`.`roles` (`role_id`, `first_name`) VALUES ('2', 'buyer');
+
+-- inserisco gli utenti
+INSERT INTO `caffebodb`.`user` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('1', 'seller', 'seller', 'seller@gmail.com', 'seller', 'Via Seller 01', '123', '1');
+INSERT INTO `caffebodb`.`user` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('2', 'buyer', 'buyer', 'buyer@gmail.com', 'buyer', 'Via Buyer 02', '456', '2');
+
