e11f23354788d7ce0f5e2c58887db3286182d6e4
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-26 23:01:41 +0100

    Table names are case sensitive

--- db/dump.sql
+++ db/dump.sql
@@ -119,10 +119,10 @@ CREATE TABLE
 -- -- riempio il db
 
 -- inserisco i ruoli
-INSERT INTO `caffebodb`.`roles` (`role_id`, `first_name`) VALUES ('1', 'seller');
-INSERT INTO `caffebodb`.`roles` (`role_id`, `first_name`) VALUES ('2', 'buyer');
+INSERT INTO `Roles` (`role_id`, `first_name`) VALUES ('1', 'seller');
+INSERT INTO `Roles` (`role_id`, `first_name`) VALUES ('2', 'buyer');
 
 -- inserisco gli utenti
-INSERT INTO `caffebodb`.`user` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('1', 'seller', 'seller', 'seller@gmail.com', 'seller', 'Via Seller 01', '123', '1');
-INSERT INTO `caffebodb`.`user` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('2', 'buyer', 'buyer', 'buyer@gmail.com', 'buyer', 'Via Buyer 02', '456', '2');
+INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('1', 'seller', 'seller', 'seller@gmail.com', 'seller', 'Via Seller 01', '123', '1');
+INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('2', 'buyer', 'buyer', 'buyer@gmail.com', 'buyer', 'Via Buyer 02', '456', '2');
 
