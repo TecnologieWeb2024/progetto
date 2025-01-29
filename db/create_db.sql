297a27a46ed80121548da09d36baa5af2f78a873
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-29 20:15:30 +0100

    Separate db creation from db population sql scripts

--- db/create_db.sql
+++ db/create_db.sql
@@ -113,18 +113,3 @@ CREATE TABLE
         FOREIGN KEY (product_id) REFERENCES Product (product_id) ON DELETE CASCADE,
         UNIQUE (user_id, product_id) -- Ensures a user cannot have duplicate entries for the same product
     ) ENGINE = InnoDB;
-    
-
-
-
-
--- -----------------------------------------
--- -- riempio il db
--- inserisco i ruoli
-INSERT INTO `Roles` (`role_id`, `first_name`) VALUES ('1', 'seller');
-INSERT INTO `Roles` (`role_id`, `first_name`) VALUES ('2', 'buyer');
-
--- inserisco gli utenti
-INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('1', 'seller', 'seller', 'seller@gmail.com', 'seller', 'Via Seller 01', '123', '1');
-INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `email`, `passwordHash`, `address`, `phone_number`, `role`) VALUES ('2', 'buyer', 'buyer', 'buyer@gmail.com', 'buyer', 'Via Buyer 02', '456', '2');
-
--- db/inserisci_dati.sql
+++ db/inserisci_dati.sql
@@ -1,1 +1,59 @@
--- TODO
+-- -----------------------------------------
+-- -- riempio il db
+-- inserisco i ruoli
+INSERT INTO
+    `Roles` (`role_id`, `first_name`)
+VALUES
+    ('1', 'seller');
+
+INSERT INTO
+    `Roles` (`role_id`, `first_name`)
+VALUES
+    ('2', 'buyer');
+
+-- inserisco gli utenti
+INSERT INTO
+    `User` (
+        `user_id`,
+        `first_name`,
+        `last_name`,
+        `email`,
+        `passwordHash`,
+        `address`,
+        `phone_number`,
+        `role`
+    )
+VALUES
+    (
+        '1',
+        'seller',
+        'seller',
+        'seller@gmail.com',
+        'seller',
+        'Via Seller 01',
+        '123',
+        '1'
+    );
+
+INSERT INTO
+    `User` (
+        `user_id`,
+        `first_name`,
+        `last_name`,
+        `email`,
+        `passwordHash`,
+        `address`,
+        `phone_number`,
+        `role`
+    )
+VALUES
+    (
+        '2',
+        'buyer',
+        'buyer',
+        'buyer@gmail.com',
+        'buyer',
+        'Via Buyer 02',
+        '456',
+        '2'
+    );
