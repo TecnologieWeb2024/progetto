2ca43817325318d816fe0830457f063750f947fe
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-30 19:09:44 +0100

    Better session message handling

--- authenticate.php
+++ authenticate.php
@@ -5,13 +5,8 @@ $authenticator = new AuthenticationHelper($dbh);
 
 $authenticationSuccess = $authenticator->login();
 
-if($authenticationSuccess['success'] === true) {
-    $_SESSION['auth_success'] = true;
-    $_SESSION['auth_message'] = $authenticationSuccess['message'];
-} else {
-    $_SESSION['auth_success'] = false;
-    $_SESSION['auth_message'] = $authenticationSuccess['message'];
-}
+$_SESSION['auth']['success'] = $authenticationSuccess['success'];
+$_SESSION['auth']['message'] = $authenticationSuccess['message'];
 
 header('Location: index.php');
 ?>
--- register.php
+++ register.php
@@ -1,27 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once('core/registrationHelper.php');
 require_once('bootstrap.php');
 $registrationHelper = new RegistrationHelper($dbh);
 
-try {
-    $registrationResult = $registrationHelper->register();
-} catch (Exception $e) {
-    // In caso di errore, salva l'errore nella sessione e fai il redirect
-    $_SESSION['error_message'] = 'Si è verificato un errore: ' . $e->getMessage();
-}
+$registrationResult = $registrationHelper->register();
 
 // Verifica se la registrazione ha avuto successo
-if ($registrationResult['success'] === true) {
-    $_SESSION['registration_success'] = $registrationResult['message'];
-} else {
-    if (isset($_SESSION['registration_errors'])) {
-        unset($_SESSION['registration_errors']);
-    }
-    // Salva gli errori nella sessione e fai il redirect alla pagina di registrazione
-    $_SESSION['registration_errors'] = $registrationResult['errors'];
-}
+$_SESSION['registration']['success'] = $registrationResult['success'];
+$_SESSION['registration']['message'] = $registrationResult['message'];
 
 header('Location: index.php');
+?>
