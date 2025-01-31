97443bc2eb4c91c9456116ddeb2ea2166782d5be
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-01-31 20:06:58 +0100

    Retreive recent orders from db

--- template/landing-page.php
+++ template/landing-page.php
@@ -4,48 +4,30 @@
         <section>
             <div class="container d-flex justify-content-center">
                 <div class="row m-2 bg-white border border-3 p-4 rounded">
-                    <h2>Ordini recenti</h2>
                     <?php
-                    // TODO: Ovviamente, i dati devono essere recuperati dal database. Recupera gli ultimi 4 ordini
-                    $orders = [
-                        ["products" => [
-                            ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
-                        ], "total" => "3.00", "date" => "2022-01-01"],
-                        ["products" => [
-                            ["name" => "Product 2", "price" => "2.00", "image" => "./assets/img/image1.jpg"],
-                            ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"]
-                        ], "total" => "3.00", "date" => "2022-01-02"],
-                        [
-                            "products" => [
-                                ["name" => "Product 1", "price" => "1.00", "image" => "./assets/img/image1.jpg"],
-                                ["name" => "Product 3", "price" => "3.00", "image" => "./assets/img/image1.jpg"],
-                                ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"],
-                                ["name" => "Product 5", "price" => "5.00", "image" => "./assets/img/image1.jpg"]
-                            ],
-                            "total" => "4.00",
-                            "date" => "2022-01-03"
-                        ]
-                    ];
+                    require_once('bootstrap.php');
+                    $orders = $dbh->getOrders($_SESSION['customer']['user_id'], 3);
                     ?>
-                    <?php foreach ($orders as $order): ?>
+                    <h2>Ordini recenti</h2>
+                    <?php foreach ($orders as $order):
+                        $products = $dbh->getOrderProducts($order['order_id']);
+                    ?>
                         <div class="col-md-4 p-2">
                             <div class="card mb-4 h-100">
                                 <div class="card-header border border-0">
-                                    <!-- NOTE: Il link deve puntare a order.php?id=... -->
+                                    <!-- NOTE: Il link deve puntare a order.php?id=... o qualcosa di simile -->
                                     <a href="#">
-                                        <p class="card-title text-center">Ordinato il: <?php echo $order['date']; ?></p>
+                                        <p class="card-title text-center">Ordinato il: <?php $date = DateTime::createFromFormat('Y-m-d H:i:s', $order['order_date']);
+                                                                                        echo $date->format('d/m/Y'); ?></p>
+
                                     </a>
                                 </div>
                                 <div class="card-body border border-0">
                                     <div class="row">
                                         <?php for ($i = 0; $i < 4; $i++): ?>
                                             <div class="col-6 mb-2">
-                                                <?php if (isset($order['products'][$i])): ?>
-                                                    <?php if ($i == 3 && count($order['products']) > 3): ?>
+                                                <?php if (count($products) > 0): ?>
+                                                    <?php if ($i == 3 && count($products) > 3): ?>
                                                         <a href="#" class="d-block h-100">
                                                             <div class="card h-100 border border-0">
                                                                 <div class="card-body text-center d-flex align-items-center justify-content-center">
@@ -54,7 +36,7 @@
                                                             </div>
                                                         </a>
                                                     <?php else: ?>
-                                                        <img src="<?php echo $order['products'][$i]['image']; ?>" class="img-fluid" alt="<?php echo $order['products'][$i]['name']; ?>">
+                                                        <img src="<?php echo $products[$i]['product_image']; ?>" class="img-fluid" alt="<?php echo $products[$i]['product_name']; ?>">
                                                     <?php endif; ?>
                                                 <?php endif; ?>
                                             </div>
