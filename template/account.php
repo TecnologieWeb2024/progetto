27d3b344c1dae74c09f05ea2a1d250313d2b2742
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-01 02:16:04 +0100

    Create basic account options page

---
+++ template/account.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('bootstrap.php');
+$user = $dbh->getUserInfo($_SESSION['customer']['user_id']);
+?>
+<h1 class="text-center">Impostazioni Account</h1>
+<div class="container col-md-4 mt-4">
+    <form action="update_account.php" method="post">
+        <div class="form-group">
+            <label class="mt-2" for="first_name">Nome:</label>
+            <input type="text" class="form-control mt-2" id="first_name" value="<?php echo htmlspecialchars($user['first_name']); ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label class="mt-2" for="last_name">Cognome:</label>
+            <input type="text" class="form-control mt-2" id="last_name" value="<?php echo htmlspecialchars($user['last_name']); ?>" readonly>
+        </div>
+        <div class="form-group">
+            <label class="mt-2" for="email">Email:</label>
+            <input type="email" class="form-control mt-2" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
+        </div>
+        <h6 class="mt-4 text-danger text-center">Modifica Password</h6>
+        <div class="form-group">
+            <label for="password">Nuova Password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Conferma Password:</label>
+            <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+        <button type="submit" class="btn btn-primary w-100 mt-4">Update</button>
+    </form>
+</div>
