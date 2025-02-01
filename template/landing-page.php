1a03e7cb2b55525fdfde74a9d5c3e3bf94c2bc66
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-01 02:15:11 +0100

    Only show Recent Orders when there is at least one order

--- template/landing-page.php
+++ template/landing-page.php
@@ -3,50 +3,52 @@
         <h1 class="text-center">Bentornato, <?php echo $_SESSION['customer']['first_name'] ?></h1>
         <section>
             <div class="container d-flex justify-content-center">
-                <div class="row m-2 bg-white border border-3 p-4 rounded">
-                    <?php
-                    require_once('bootstrap.php');
-                    $orders = $dbh->getOrders($_SESSION['customer']['user_id'], 3);
-                    ?>
-                    <h2>Ordini recenti</h2>
-                    <?php foreach ($orders as $order):
-                        $products = $dbh->getOrderProducts($order['order_id']);
-                    ?>
-                        <div class="col-md-4 p-2">
-                            <div class="card mb-4 h-100">
-                                <div class="card-header border border-0">
-                                    <!-- NOTE: Il link deve puntare a order.php?id=... o qualcosa di simile -->
-                                    <a href="#">
-                                        <p class="card-title text-center">Ordinato il: <?php $date = DateTime::createFromFormat('Y-m-d H:i:s', $order['order_date']);
-                                                                                        echo $date->format('d/m/Y'); ?></p>
+                <?php
+                require_once('bootstrap.php');
+                $orders = $dbh->getOrders($_SESSION['customer']['user_id'], 3);
+                if (count($orders) > 0):
+                ?>
+                    <div class="row m-2 bg-white border border-3 p-4 rounded">
+                        <h2>Ordini recenti</h2>
+                        <?php foreach ($orders as $order):
+                            $products = $dbh->getOrderProducts($order['order_id']);
+                        ?>
+                            <div class="col-md-4 p-2">
+                                <div class="card mb-4 h-100">
+                                    <div class="card-header border border-0">
+                                        <!-- NOTE: Il link deve puntare a order.php?id=... o qualcosa di simile -->
+                                        <a href="#">
+                                            <p class="card-title text-center">Ordinato il: <?php $date = DateTime::createFromFormat('Y-m-d H:i:s', $order['order_date']);
+                                                                                            echo $date->format('d/m/Y'); ?></p>
 
-                                    </a>
-                                </div>
-                                <div class="card-body border border-0">
-                                    <div class="row">
-                                        <?php for ($i = 0; $i < 4; $i++): ?>
-                                            <div class="col-6 mb-2">
-                                                <?php if (count($products) > 0): ?>
-                                                    <?php if ($i == 3 && count($products) > 3): ?>
-                                                        <a href="#" class="d-block h-100">
-                                                            <div class="card h-100 border border-0">
-                                                                <div class="card-body text-center d-flex align-items-center justify-content-center">
-                                                                    Altro...
+                                        </a>
+                                    </div>
+                                    <div class="card-body border border-0">
+                                        <div class="row">
+                                            <?php for ($i = 0; $i < 4; $i++): ?>
+                                                <div class="col-6 mb-2">
+                                                    <?php if (count($products) > 0): ?>
+                                                        <?php if ($i == 3 && count($products) > 3): ?>
+                                                            <a href="#" class="d-block h-100">
+                                                                <div class="card h-100 border border-0">
+                                                                    <div class="card-body text-center d-flex align-items-center justify-content-center">
+                                                                        Altro...
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                        </a>
-                                                    <?php else: ?>
-                                                        <img src="<?php echo $products[$i]['product_image']; ?>" class="img-fluid" alt="<?php echo $products[$i]['product_name']; ?>">
+                                                            </a>
+                                                        <?php else: ?>
+                                                            <img src="<?php echo $products[$i]['product_image']; ?>" class="img-fluid" alt="<?php echo $products[$i]['product_name']; ?>">
+                                                        <?php endif; ?>
                                                     <?php endif; ?>
-                                                <?php endif; ?>
-                                            </div>
-                                        <?php endfor; ?>
+                                                </div>
+                                            <?php endfor; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </section>
     <?php else: ?>
