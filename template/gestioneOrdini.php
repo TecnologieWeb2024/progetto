07217e9ccb33a32c2ab7a1a679202de004bb22ce
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-02-02 23:17:04 +0100

    addede placeholder nav buttons for seller

--- template/gestioneOrdini.php
+++ template/gestioneOrdini.php
@@ -6,3 +6,18 @@ require_once('bootstrap.php');
 <h1 class="text-center">Gestione Ordini</h1>
 <div class="container col-md-4 mt-4">
 </div>
+
+
+<div>
+
+    <?php 
+    $products = $dbh->getAllProducts();
+    $totalProducts = count($products);
+    ?>
+    <!-- // TODO creare query per gli ordini
+    // TODO totale ordini
+    // TODO totale ordini da evadere
+    // TODO totale ordini evasi
+    // TODO totale ordini conclusi -->
+    <h5>Nel DB ci sono <?php echo $totalProducts ?> prodotti</h5>
+</div>
--- template/gestioneProdotti.php
+++ template/gestioneProdotti.php
@@ -6,3 +6,13 @@ require_once('bootstrap.php');
 <h1 class="text-center">Gestione Prodotti</h1>
 <div class="container col-md-4 mt-4">
 </div>
+
+
+<div>
+
+    <?php 
+    $products = $dbh->getAllProducts();
+    $totalProducts = count($products);
+    ?>
+    <h5>Nel DB ci sono <?php echo $totalProducts ?> prodotti</h5>
+</div>
