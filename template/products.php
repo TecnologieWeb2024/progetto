61d8c8a3568b1bab9962da16aa1fb6cae6bc28a5
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-04 21:04:32 +0100

    Improve product card body price alignment

--- template/products.php
+++ template/products.php
@@ -24,41 +24,47 @@
                 <div class="col-sm-6 col-md-4">
                     <div class="card mb-4">
                         <img src="<?php echo $product['image'] ?>" class="card-img-top" alt="<?php echo $product['product_name'] ?>">
-                        <div class="card-body border border-0 position-relative" style="min-height: 10em">
-                            <!-- NOTE: L'altezza minima è impostata per supportare titoli che occupano più di una riga -->
+                        <div class="container d-flex flex-column m-0 p-0">
+                            <div class="col-12">
+                                <div class="card-body d-flex flex-column" style="min-height: 10em">
+                                    <!-- Titolo e link per il modale -->
+                                    <a type="button"
+                                        class="h4 card-title btn-link"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#productModal"
+                                        data-name="<?php echo htmlspecialchars($product['product_name']); ?>"
+                                        data-price="<?php echo $product['price']; ?>"
+                                        data-image="<?php echo $product['image']; ?>"
+                                        data-description="<?php echo htmlspecialchars($product['product_description'] ?? ''); ?>"
+                                        data-max="<?php echo $product['stock']; ?>">
+                                        <?php echo $product['product_name']; ?>
+                                    </a>
 
-                            <a class="h3 card-title btn-link"
-                                data-bs-toggle="modal"
-                                data-bs-target="#productModal"
-                                data-name="<?php echo htmlspecialchars($product['product_name']); ?>"
-                                data-price="<?php echo $product['price']; ?>"
-                                data-image="<?php echo $product['image']; ?>"
-                                data-description="<?php echo htmlspecialchars($product['product_description'] ?? ''); ?>"
-                                data-max="<?php echo $product['stock']; ?>">
-
-                                <?php echo $product['product_name'] ?>
-
-                            </a>
-                            <p class="card-text h3 text-end position-absolute m-4" style="bottom: 0; right: 0;"><?php echo $product['price'] ?>€</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="container d-flex justify-content-center">
-                                <div class="d-flex align-items-center justify-content-between gap-3">
-                                    <div class="d-flex align-items-center border border-1 rounded ms-2 me-2">
-                                        <button type="button" class="quantity-left-minus btn btn-secondary btn-number rounded rounded-0 rounded-start" data-type="minus" data-field="">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-                                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"></path>
-                                            </svg>
-                                        </button>
-                                        <label for="quantity-<?php echo $product['product_id'] ?>" class="visually-hidden">Quantità</label>
-                                        <input type="number" id="quantity-<?php echo $product['product_id'] ?>" name="quantity" class="form-control text-center rounded rounded-0" value="1" min="1" max="<?php echo $product['stock'] ?>" style="width:3em">
-                                        <button type="button" class="quantity-right-plus btn btn-secondary btn-number rounded rounded-0 rounded-end" data-type="plus" data-field="">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
-                                            </svg>
-                                        </button>
+                                    <!-- Sezione prezzo spinta in fondo grazie a mt-auto -->
+                                    <div class="mt-auto">
+                                        <p class="card-text h3 text-end"><?php echo $product['price']; ?>€</p>
                                     </div>
-                                    <a href="#" title="add-to-cart" class="btn btn-primary float-end w-25 me-2"><em class="fa fa-cart-plus"></em></a>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="container d-flex justify-content-center">
+                                    <div class="d-flex align-items-center justify-content-between gap-3">
+                                        <div class="d-flex align-items-center border border-1 rounded ms-2 me-2">
+                                            <button type="button" class="quantity-left-minus btn btn-secondary btn-number rounded rounded-0 rounded-start" data-type="minus" data-field="">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"></path>
+                                                </svg>
+                                            </button>
+                                            <label for="quantity-<?php echo $product['product_id'] ?>" class="visually-hidden">Quantità</label>
+                                            <input type="number" id="quantity-<?php echo $product['product_id'] ?>" name="quantity" class="form-control text-center rounded rounded-0" value="1" min="1" max="<?php echo $product['stock'] ?>" style="width:3em">
+                                            <button type="button" class="quantity-right-plus btn btn-secondary btn-number rounded rounded-0 rounded-end" data-type="plus" data-field="">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        <a href="#" title="add-to-cart" class="btn btn-primary float-end w-25 me-2"><em class="fa fa-cart-plus"></em></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
