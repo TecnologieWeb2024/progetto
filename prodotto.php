4ba85bb7e18952fc9ca4042ca6553a9646bf5837
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-06 17:34:10 +0100

    Validate product's modal by using placeholders for empty fields

--- prodotto.php
+++ prodotto.php
@@ -5,12 +5,12 @@
                 <div class="col-12">
                     <div class="container modal-body d-flex p-0 pt-4">
                         <div class="col-md-6 col-4">
-                            <img id="modalProductImage" src="" class="img-fluid" alt="">
+                            <img id="modalProductImage" src="assets/image1.jpg" class="img-fluid" alt="placeholder">
                         </div>
                         <div class="col-md-6 col-8 d-flex flex-column">
                             <div class="row">
-                                <h3 id="modalProductName"></h3>
-                                <p id="modalProductDescription"></p>
+                                <h3 id="modalProductName">Placeholder</h3>
+                                <p id="modalProductDescription">Placeholder</p>
                             </div>
                             <div class="row mt-auto">
                                 <p id="modalProductPrice" class="text-end h3"></p>
@@ -27,7 +27,7 @@
                                         </svg>
                                     </button>
                                     <label for="quantity" class="visually-hidden">Quantità</label>
-                                    <input type="number" id="quantity" name="quantity" class="form-control text-center rounded rounded-0" value="1" min="1" max="<?php echo $product['stock'] ?>" style="width:3em">
+                                    <input type="number" id="quantity" name="quantity" class="form-control text-center rounded rounded-0" value="1" min="1" max="999" style="width:3em">
                                     <button type="button" class="quantity-right-plus btn btn-secondary btn-number rounded rounded-0 rounded-end" data-type="plus" data-field="">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
