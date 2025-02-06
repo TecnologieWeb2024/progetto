250cf931601f6fa2fd2f7405f3211628c358672a
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-06 12:22:15 +0100

    Add alerts for adding to cart

--- addToCartHandler.php
+++ addToCartHandler.php
@@ -15,6 +15,7 @@ set_exception_handler(function ($exception) {
 require_once('bootstrap.php');
 
 if (isUserLoggedIn() === false) {
+    $_SESSION['error_message'] = 'Utente non autenticato';
     header('Content-Type: application/json');
     echo json_encode([
         'success' => false,
@@ -27,9 +28,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $productId = $_POST['product_id'] ?? null;
     $quantity = $_POST['quantity'] ?? 1;
 
-    // Assicurati di validare e sanitizzare i dati in ingresso
     if ($productId) {
         $result = $dbh->addToCart($_SESSION['customer']['user_id'], $productId, $quantity);
+        if($result === false) {
+            $_SESSION['error_message'] = 'Errore durante l\'aggiunta al carrello: '. $result['message'];
+        } else {
+            $_SESSION['success_message'] = 'Prodotto aggiunto al carrello';
+        }
         header('Content-Type: application/json');
         echo json_encode([
             'success' => $result['success'],
@@ -41,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header('Content-Type: application/json');
         echo json_encode([
             'success' => false,
-            'message' => 'Dati non validi'
+            'message' => 'id prodotto non valido'
         ]);
     }
 }
