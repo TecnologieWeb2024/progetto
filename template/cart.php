04975cfc12e24970628f816e09399b27d5f2661e
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-06 17:41:30 +0100

    Validate html and improve accessibility

--- template/cart.php
+++ template/cart.php
@@ -9,7 +9,7 @@
         $result = $dbh->getCartProducts($_SESSION['customer']['user_id']);
         $cartProducts = $result['products'];
         $totalPrice = $result['total_price'];
-        require_once('prodotto.php')
+        require_once('productModal.php')
         ?>
         <div class="row">
             <div class="col-12 mb-4">
@@ -20,7 +20,7 @@
         <?php foreach ($cartProducts as $product): ?>
             <div class="row border rounded p-3 m-1 bg-light justify-content-center align-items-center">
                 <div class="col-6 col-md-2 text-center text-md-start">
-                    <img src="<?php echo $product['image'] ?>" class="img-fluid" alt="">
+                    <img src="<?php echo $product['image']; ?>" class="img-fluid" alt="<?php echo $product['product_name']; ?> image">
                 </div>
                 <!-- Product Details (Name, Description, Price) -->
                 <div class="col-12 col-md-4 text-center text-md-start">
@@ -35,8 +35,8 @@
                         data-max="<?php echo $product['stock']; ?>">
                         <?php echo $product['product_name']; ?>
                     </a>
-                    <p class="d-none d-md-block"><?php echo $product['product_description'] ?></p>
-                    <p class="d-md-none fw-bold h3"><?php echo $product['price'] ?>€</p>
+                    <p class="d-none d-md-block"><?php echo $product['product_description']; ?></p>
+                    <p class="d-md-none fw-bold h3"><?php echo $product['price']; ?>€</p>
 
 
                 </div>
@@ -53,15 +53,15 @@
                                 <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"></path>
                             </svg>
                         </button>
-                        <label for="quantity-<?php echo $product['product_id'] ?>" class="visually-hidden">Quantità</label>
-                        <input type="number" id="quantity-<?php echo $product['product_id'] ?>" name="quantity" class="form-control text-center rounded rounded-0" value="<?php echo $product['quantity']; ?>" min="1" max="<?php echo $product['stock'] ?>" style="width:3em">
+                        <label for="quantity-<?php echo $product['product_id']; ?>" class="visually-hidden">Quantità</label>
+                        <input type="number" id="quantity-<?php echo $product['product_id']; ?>" name="quantity" class="form-control text-center rounded rounded-0" value="<?php echo $product['quantity']; ?>" min="1" max="<?php echo $product['stock']; ?>" style="width:3em">
                         <button type="button" class="quantity-right-plus btn btn-secondary btn-number rounded rounded-0 rounded-end" data-type="plus" data-field="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
                             </svg>
                         </button>
                     </div>
-                    <a href="#" class="btn btn-danger" data-product-id="<?php echo $product['product_id'] ?>">
+                    <a href="#" class="btn btn-danger" title="remove-product" data-product-id="<?php echo $product['product_id'] ?>">
                         <em class="fa fa-trash"></em>
                     </a>
                 </div>
