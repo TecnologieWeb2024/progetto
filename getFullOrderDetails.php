1198b792f2446f4bf678d91af4b5f95d7f62aa79
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-05-07 16:58:49 +0200

    Remove error reporting on frontend

--- getFullOrderDetails.php
+++ getFullOrderDetails.php
@@ -1,8 +1,5 @@
 <?php
 require_once('bootstrap.php');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $orderId = (int)$_POST['order_id'];
 header('Content-Type: application/json');
 
