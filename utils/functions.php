23726fc17ca74ce8d1ec4c7abaf338db1b8bbbf7
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-05-10 23:12:21 +0200

    gestione colore badge

--- utils/functions.php
+++ utils/functions.php
@@ -12,4 +12,15 @@ function isUserSeller()
 {
     return array_key_exists('seller', $_SESSION);
 }
+
+function getBadgeClassFromStateId(int $stateId): string {
+    return match (true) {
+        in_array($stateId, [1, 2]) => 'badge-status-warning',
+        in_array($stateId, [3]) => 'badge-status-indigo-600',
+        in_array($stateId, [4, 5, 6]) => 'badge-status-success',
+        default => 'badge-status-danger',
+    };
+}
+
+
 ?>
