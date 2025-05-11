24534d88577debecc52956a1ce47860a938c4242
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-05-11 18:07:43 +0200

    Recent orders cards now open the order detail

--- template/landing-page.php
+++ template/landing-page.php
@@ -16,30 +16,27 @@
                             $products = $dbh->getOrderProducts($order['order_id']);
                         ?>
                             <div class="col-md-4 p-2">
-                                <div class="card mb-4 h-100">
+                                <div class="card mb-4 h-100 hover-darken" 
+                                     data-bs-toggle="modal"
+                                     data-bs-target="#orderModal"
+                                     data-order-id="<?php echo $order['order_id']; ?>"
+                                     style="cursor: pointer;">
                                     <div class="card-header border border-0">
-                                        <!-- NOTE: Il link deve puntare a order.php?id=... o qualcosa di simile -->
-                                        <a href="#"
-                                            data-bs-toggle="modal"
-                                            data-bs-target="#orderModal"
-                                            data-order-id="<?php echo $order['order_id']; ?>">
-                                            <p class="card-title text-center">
-                                                Ordinato il:
-                                                <?php
-                                                $date = DateTime::createFromFormat('Y-m-d H:i:s', $order['order_date']);
-                                                echo $date->format('d/m/Y');
-                                                ?>
-                                                <br>
-                                                <?php
-                                                $orderStatus = $dbh->getOrderstatus($order['order_id']);
-                                                $badgeClass = getBadgeClassFromstatusId($orderStatus['order_status_id']);
-                                                ?>
-                                                <span class="badge <?php echo $badgeClass; ?>">
-                                                    <?php echo $orderStatus['descrizione']; ?>
-                                                </span>
-                                            </p>
-
-                                        </a>
+                                        <p class="card-title text-center">
+                                            Ordinato il:
+                                            <?php
+                                            $date = DateTime::createFromFormat('Y-m-d H:i:s', $order['order_date']);
+                                            echo $date->format('d/m/Y');
+                                            ?>
+                                            <br>
+                                            <?php
+                                            $orderStatus = $dbh->getOrderstatus($order['order_id']);
+                                            $badgeClass = getBadgeClassFromstatusId($orderStatus['order_status_id']);
+                                            ?>
+                                            <span class="badge <?php echo $badgeClass; ?>">
+                                                <?php echo $orderStatus['descrizione']; ?>
+                                            </span>
+                                        </p>
                                     </div>
                                     <div class="card-body border border-0">
                                         <div class="row">
@@ -48,15 +45,13 @@
                                                     <?php if (count($products) > 0): ?>
 
                                                         <?php if ($i == 3 && count($products) > 3): ?>
-                                                            <a href="#" class="d-block h-100">
-                                                                <div class="card h-100 border border-0">
-                                                                    <div class="card-body text-center d-flex align-items-center justify-content-center">
-                                                                        Altro...
-                                                                    </div>
+                                                            <div class="card h-100 border border-0">
+                                                                <div class="card-body text-center d-flex align-items-center justify-content-center">
+                                                                    Altro...
                                                                 </div>
-                                                            </a>
+                                                            </div>
                                                         <?php else: ?>
-                                                            <img src="<?php echo $products[$i]['image']; ?>" class="img-fluid" alt="<?php echo $products[$i]['product_name']; ?>">
+                                                            <img src="<?php echo $products[$i]['image']; ?>" class="img-fluid rounded" alt="<?php echo $products[$i]['product_name']; ?>">
                                                         <?php endif; ?>
                                                     <?php endif; ?>
                                                 </div>
