83cc9cf318038cfcdfd31af597a0446275b7cfe2
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-06-08 17:55:41 +0200

    Add cancel order feature in seller dash

--- js/orderModal.js
+++ js/orderModal.js
@@ -28,3 +28,34 @@ document
         alert("Errore durante la richiesta.");
       });
   });
+
+  document
+    .getElementById("btnCancelOrder")
+    .addEventListener("click", function () {
+      const orderId = document.getElementById("modalOrderId").textContent;
+
+      fetch("utils/updateOrderStatus.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          order_id: orderId,
+          new_status: 7, // Cancellato
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            alert("Ordine aggiornato con successo");
+            // reload the page to reflect changes
+            location.reload();
+          } else {
+            alert("Errore: " + data.message);
+          }
+        })
+        .catch((err) => {
+          console.error("Errore AJAX:", err);
+          alert("Errore durante la richiesta.");
+        });
+    });
