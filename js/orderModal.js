933db63349e2427124696e290d573a5050e5531d
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-06-08 18:01:46 +0200

    refactor changeOrderStatus

--- js/orderModal.js
+++ js/orderModal.js
@@ -1,33 +1,31 @@
-document
-  .getElementById("btnChangeOrderStatus")
-  .addEventListener("click", function () {
-    const orderId = document.getElementById("modalOrderId").textContent;
+function changeOrderStatus () {
+  const orderId = document.getElementById("modalOrderId").textContent;
 
-    fetch("utils/updateOrderStatus.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        order_id: orderId,
-        new_status: 4, // In preparazione
-      }),
+  fetch("utils/updateOrderStatus.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      order_id: orderId,
+      new_status: 4, // In preparazione
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("Ordine aggiornato con successo");
+        // reload the page to reflect changes
+        location.reload();
+      } else {
+        alert("Errore: " + data.message);
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          alert("Ordine aggiornato con successo");
-          // reload the page to reflect changes
-          location.reload();
-        } else {
-          alert("Errore: " + data.message);
-        }
-      })
-      .catch((err) => {
-        console.error("Errore AJAX:", err);
-        alert("Errore durante la richiesta.");
-      });
-  });
+    .catch((err) => {
+      console.error("Errore AJAX:", err);
+      alert("Errore durante la richiesta.");
+    });
+}
 
   document
     .getElementById("btnCancelOrder")
