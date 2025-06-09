d46cb0077f9bd3b4085c83a10ba9092d8ed4042d
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-06-09 17:59:15 +0200

    Complete contacts page

--- template/contacts.php
+++ template/contacts.php
@@ -1,10 +1,13 @@
-<div class="container mb-4">
+<div class="container mb-4 justify-content-center text-center">
     <h1 class="mb-4">Contatti</h1>
 
-    <p>Se hai domande, non esitare a contattarci attraverso i seguenti metodi:</p>
+    <p>Alessandro Monticelli, Sergio Dobrianskiy</p>
 
     <h2 class="mt-4">Email</h2>
-    <p>Puoi inviarci un'email a <a href="mailto:info@example.com">info@example.com</a></p>
+    <p>Puoi inviarci un'email a:
+        <br><a href="mailto:alessandr.monticell4@studio.unibo.it">alessandr.monticell4@studio.unibo.it</a>
+        <br><a href="mailto:sergio.dobrianskiy@studio.unibo.it">sergio.dobrianskiy@studio.unibo.it</a>
+    </p>
 
     <h2 class="mt-4">Telefono</h2>
     <p>Chiamaci al: (123) 456-7890</p>
@@ -14,33 +17,4 @@
     <address>
         Via Dell'Università 50, Cesena, FC
     </address>
-
-    <h2 class="mt-4">Modulo di Contatto</h2>
-    <form action="#" method="post" class="needs-validation" novalidate>
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome:</label>
-            <input type="text" id="name" name="name" class="form-control" required>
-            <div class="invalid-feedback">
-                Per favore inserisci il tuo nome.
-            </div>
-        </div>
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" id="email" name="email" class="form-control" required>
-            <div class="invalid-feedback">
-                Per favore inserisci un indirizzo email valido.
-            </div>
-        </div>
-
-        <div class="mb-3">
-            <label for="message" class="form-label">Messaggio:</label>
-            <textarea id="message" name="message" class="form-control" rows="5" required></textarea>
-            <div class="invalid-feedback">
-                Per favore inserisci il tuo messaggio.
-            </div>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Invia</button>
-    </form>
 </div>
