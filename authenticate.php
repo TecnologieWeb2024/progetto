71dce862dec2967e59bf7b94b4e099c387cc9579
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-06-09 17:57:51 +0200

    Bug fix: redirect to homepage on auth failure

--- authenticate.php
+++ authenticate.php
@@ -15,5 +15,7 @@ if (isUserCustomer()) {
     header('Location: index.php');
 } else if (isUserSeller()) {
     header('Location: index.php?page=seller');
+} else {
+    header('Location: index.php');
 }
 ?>
